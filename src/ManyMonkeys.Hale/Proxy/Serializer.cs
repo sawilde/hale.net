611c02573bc38f20d6a8ff5c6d934d47a513100a
Shaun Wilde <shaun_wilde@hotmail.com>
2017-12-27 08:39:24 +1100

    spike - use interceptors

---
+++ src/ManyMonkeys.Hale/Proxy/Serializer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Reflection;
+using Castle.DynamicProxy;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ManyMonkeys.Hale.Proxy
+{
+    public interface IHaleObject
+    {
+        string SerializeProxy();
+    }
+
+    public static class Serializer
+    {
+        public static T DeserializeAsProxy<T>(this string wire) where T : class, IHaleObject
+        {
+            var obj = JsonConvert.DeserializeObject(wire);
+
+            var generator = new ProxyGenerator();
+            var proxy = generator.CreateInterfaceProxyWithoutTarget<T>(new HaleObjectInterceptor((JObject)obj));
+
+            return proxy; 
+        }
+    }
+
+    public class HaleObjectInterceptor : IInterceptor
+    {
+        private readonly JObject _obj;
+
+        public HaleObjectInterceptor(JObject obj)
+        {
+            _obj = obj;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            string GetPropertySerializationName(out PropertyInfo property)
+            {
+                var name = invocation.Method.Name.Substring(4);
+                var declaringType = invocation.Method.DeclaringType;
+                property = declaringType.GetProperty(name);
+                var attr = property.GetCustomAttribute<JsonPropertyAttribute>();
+                if (attr != null)
+                {
+                    name = attr.PropertyName;
+                }
+
+                return name;
+            }
+
+            if (invocation.Method.IsSpecialName && invocation.Method.Name.StartsWith("get_"))
+            {
+                var name = GetPropertySerializationName(out var property);
+                invocation.ReturnValue = Convert.ChangeType(_obj[name], property.PropertyType);
+            }
+
+            if (invocation.Method.IsSpecialName && invocation.Method.Name.StartsWith("set_"))
+            {
+                var name = GetPropertySerializationName(out var property);
+                _obj.Add(name, new JValue(invocation.Arguments[0]));
+            }
+
+            if (invocation.Method.Name == "SerializeProxy")
+            {
+                invocation.ReturnValue = JsonConvert.SerializeObject(_obj);
+            }
+        }
+    }
+}
---
+++ test/ManyMonkeys.Hale.Tests/ProxyTests/SerializerTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.Serialization;
+using ManyMonkeys.Hale.Proxy;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace ManyMonkeys.Hale.Tests.ProxyTests
+{
+    
+    public class SerializerTests
+    {
+        public interface IUser : IHaleObject
+        {
+            [JsonProperty(PropertyName = "name")]
+            string Name { get; set; }
+
+            //[JsonProperty(PropertyName = "age")]
+            int? Age { get; set; }
+        }
+
+        [Fact]
+        public void CanDeserializeAsProxy()
+        {
+            var source = "{\"name\":\"Arthur Dent\"}";
+            var user = source.DeserializeAsProxy<IUser>();
+
+            Assert.Equal("Arthur Dent", user.Name);
+        }
+
+        [Fact]
+        public void CanSerializeProxy()
+        {
+            var source = "{\"name\":\"Arthur Dent\"}";
+            var user = source.DeserializeAsProxy<IUser>();
+            var wire = user.SerializeProxy();
+            Assert.Equal(source, wire);
+        }
+
+        [Fact]
+        public void CanSerializeModifiedProxy()
+        {
+            var fmtSource = "{{\"name\":\"Arthur Dent\"{0}}}";
+            var user = string.Format(fmtSource, "").DeserializeAsProxy<IUser>();
+
+            var wire = user.SerializeProxy();
+            Assert.Equal(string.Format(fmtSource, ""), wire);
+
+            user.Age = 42;
+
+            wire = user.SerializeProxy();
+            Assert.Equal(string.Format(fmtSource, ",\"Age\":42"), wire);
+        }
+    }
+}
